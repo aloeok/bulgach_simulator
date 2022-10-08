49ead821b13344fcab81c3a9864a3e6d4499667f
Alexander Loenko <aloeok@mail.ru>
2022-10-08 14:24:10 +0700

    New tester - works on Linux

--- src/tester.sh
+++ src/tester.sh
@@ -1,6 +1,8 @@
 ./tests_data_merger.sh
 MERGE_RES="${?}"
 
+gcc -std=c89 -Wall -Werror -pedantic -o "ans_checker" "ans_checker.c"
+
 if [ $MERGE_RES -eq "0" ]
 then
 
@@ -9,7 +11,8 @@ do
 	
 	PROG=${PROG%$'\n'}
 	PROG=${PROG%$'\r'}
-	gcc -c -std=c89 -Wall -Werror -pedantic -lm "${PROG}.c" 2> /dev/null && gcc -Wall -o "${PROG}" "${PROG}.o" 2> /dev/null
+	#gcc -c -std=c89 -Wall -Werror -pedantic -lm "${PROG}.c" 2> /dev/null && gcc -Wall -o "${PROG}" "${PROG}.o" 2> /dev/null
+	gcc -std=c89 -Wall -Werror -pedantic -o "${PROG}" "${PROG}.c" -lm
 	COMP_RES="${?}"
 	if [ $COMP_RES -eq "0" ]
 	then
@@ -37,69 +40,81 @@ do
 			ARG_DATA=${ARG_DATA%$'\r'}
 			
 			ANS=$(echo $INPUT_DATA | timeout 5 "./${PROG}" 2>&1)
+			
 			EXIT_CODE=$(echo $?)
 			
 			if [ "${EXIT_CODE}" = "${REQ_EXIT}" ]
 			then
-				CHECK_DATA=$(echo -e "${COR_ANS}\n${ANS}")
-				echo $CHECK_DATA | timeout 3 ./ans_checker $ARG_DATA
+				CHECK_DATA=$(printf "a${COR_ANS}\n${ANS}")
+				
+				printf "${CHECK_DATA}\n" | timeout 3 ./ans_checker $ARG_DATA
 				CHECK_RES=$(echo $?)
 				
 				if [ "${REQ_EXIT}" = "1" ] && [ "${ANS}" = "" ]
 				then
-					echo "${PROG} : ${TEST_ID} : OK"
-					echo "(Tested for input: ${INPUT_DATA})"
+					echo "${PROG} : ${TEST_ID} : OK (correct input check)"
+					echo "Tested for input: ${INPUT_DATA}"
 				elif [ "${REQ_EXIT}" = "1" ]
 				then
-					echo "${PROG} : ${TEST_ID} : EXTRA OUTPUT / RUNTIME ERROR"
+					echo "${PROG} : ${TEST_ID} : FAIL (extra output / runtime error)"
 					echo "Input data: ${INPUT_DATA}"
-					echo -e "Answer:\n${ANS}"
+					echo "Answer:"
+					echo "[${ANS}]"
 					echo "Exit code: ${EXIT_CODE}"
-					echo "Correct answer: ${COR_ANS}"
+					echo "Correct answer: $[{COR_ANS}]"
 					echo "Correct exit code: ${REQ_EXIT}"
 				
 				elif [ "${CHECK_RES}" = "124" ]
 				then
-					echo "${PROG} : ${TEST_ID} : INCORRECT OUTPUT FORMAT"
+					echo "${PROG} : ${TEST_ID} : TESTER TL (incorrect output / incorrect test data)"
 					echo "Input data: ${INPUT_DATA}"
-					echo -e "Answer:\n${ANS}"
-					echo "Correct answer: ${COR_ANS}"
+					echo "Answer:"
+					echo "[${ANS}]"
+					echo "Correct answer: [${COR_ANS}]"
 				elif [ "${CHECK_RES}" = "3" ]
 				then
-					echo "${PROG} : ${TEST_ID} : INCORRECT TEST FILE DATA !!!"
+					echo "${PROG} : ${TEST_ID} : TESTER ERROR (incorrect test data)"
 					echo "Input data: ${INPUT_DATA}"
-					echo -e "Answer:\n${ANS}"
+					echo "Answer:"
+					echo "${ANS}"
 					echo "Correct answer: ${COR_ANS}"
 				elif [ "${CHECK_RES}" = "2" ]
 				then
-					echo "${PROG} : ${TEST_ID} : INCORRECT OUTPUT FORMAT"
+					echo "${PROG} : ${TEST_ID} : FAIL (incorrect output)"
 					echo "Input data: ${INPUT_DATA}"
-					echo -e "Answer:\n${ANS}"
-					echo "Correct answer: ${COR_ANS}"
+					echo "Answer:"
+					echo "[${ANS}]"
+					echo "Correct answer: [${COR_ANS}]"
 				elif [ "${CHECK_RES}" = "1" ]
 				then
-					echo "${PROG} : ${TEST_ID} : WRONG ANSWER"
+					echo "${PROG} : ${TEST_ID} : FAIL (wrong answer)"
 					echo "Input data: ${INPUT_DATA}"
-					echo -e "Answer:\n${ANS}"
+					echo "Answer:"
+					echo "${ANS}"
 					echo "Correct answer: ${COR_ANS}"
 				elif [ "${CHECK_RES}" = "0" ]
 				then
-					echo "${PROG} : ${TEST_ID} : OK"
-					echo "(Tested for input: ${INPUT_DATA})"
+					echo "${PROG} : ${TEST_ID} : OK (correct answer)"
+					echo "Tested for input: ${INPUT_DATA}"
 				else
-					echo "${PROG} : ${TEST_ID} : TESTER ERROR ${CHECK_RES}"
+					echo "${PROG} : ${TEST_ID} : TESTER ERROR ${CHECK_RES} (some real bullshit happened)"
 					echo "Input data: ${INPUT_DATA}"
-					echo -e "Answer:\n${ANS}"
-					echo "Correct answer: ${COR_ANS}"
+					echo "Answer:"
+					echo "[${ANS}]"
+					echo "Exit code: [${EXIT_CODE}]"
+					echo "Correct answer: [${COR_ANS}]"
+					echo "Correct exit code: [${REQ_EXIT}]"
+					
 				fi
 			elif [ "${EXIT_CODE}" = "124" ]
 			then
-				echo "${PROG} : ${TEST_ID} : PROGRAM WORKED > 5 SECONDS (check for infinite loops)"
+				echo "${PROG} : ${TEST_ID} : FAIL (program worked > 5 seconds (infinite loop / extra scanf / ineffective algorithm))"
 				echo "Input data: ${INPUT_DATA}"
 			else
-				echo "${PROG} : ${TEST_ID} : WRONG EXIT CODE / RUNTIME ERROR"
+				echo "${PROG} : ${TEST_ID} : WRONG EXIT CODE (or incorrect test data)"
 				echo "Input data: ${INPUT_DATA}"
-				echo -e "Answer:\n${ANS}"
+				echo "Answer:"
+				echo "${ANS}"
 				echo "Exit code: ${EXIT_CODE}"
 				echo "Correct answer: ${COR_ANS}"
 				echo "Correct exit code: ${REQ_EXIT}"
@@ -125,12 +140,14 @@ do
 
 	fi
 	rm "current_test_in_data.btdata" 2> /dev/null
+	rm "${PROG}"
 done < "prog_list.btdata"
 
 rm "all_test_data.btdata" 2> /dev/null
+rm "ans_checker" 2> /dev/null
 
 else
-echo "MERGING ERROR"
+echo "TEST DATA MERGING ERROR"
 
 fi
 
